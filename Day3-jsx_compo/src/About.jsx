2e77c18c4c8044f16825520e7b5578a0693b1652
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-06-25 20:03:18 +0530

    Day 3 - Learnt about basics of JSX components

---
+++ Day3-jsx_compo/src/About.jsx
@@ -0,0 +1,6 @@
+let About=(name)=>{
+console.log(name)
+    return <h1>Hey i am About </h1>
+}
+
+export default About;
---
+++ Day3-jsx_compo/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import About from "./About";
+
+let App=()=>{
+
+ console.log(name.width)
+
+  return (
+    <div>
+      <h1>hello</h1>
+      <h2>bye</h2>
+      <h3>vapas ao</h3>
+      <About width="300" />
+    </div>
+  );
+ 
+}
+
+export default App;
