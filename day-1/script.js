4dd3d4c9461c7589d5e7714cdd5ab370a5cfee93
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-29 20:22:59 +0530

    completed day1 of react

--- day-1/script.js
+++ day-1/script.js
@@ -13,10 +13,17 @@ const rh1=React.createElement("h1",
 
     // document.body.append(rh1); Not applicable it shows [Object][Object] in web page , reason: i cannot understand parse
 
+    const RdomElem=document.querySelector("#root");
+
+    const div=React.createElement("div",null,React.createElement("h1",null,React.createElement("span",null,"i am span from react")))
+
+    ReactDOM.createRoot(RdomElem).render(div)
+
 const realDOMelem=document.querySelector("#root");
 const rootOfReact=ReactDOM.createRoot(realDOMelem);
 rootOfReact.render(rh1);
 
+// i had used #root twice just for understanding ..it should be called only once
 
 console.log("Real Dom",h1);
 console.log("virtual Dom",rh1);
