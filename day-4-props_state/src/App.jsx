264966eb3621d1e029204fc5f9412c7627a7c6ba
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-08-06 23:08:56 +0530

    default export and normal export

--- day-4-props_state/src/App.jsx
+++ day-4-props_state/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-
+import About from "./About";
+import { one } from "./test";
+import something from "./test"
 const App=()=>{
-  return(
-    <div>
+one();
+something();
 
-    </div>
-  )
+    return(
+        <div>
+            <h1>I am simple app</h1>
+            <About/>
+        </div>
+    )
 }
 
 export default App;
---
+++ day-4-props_state/src/test.jsx
@@ -0,0 +1,17 @@
+export const one=()=>{
+    console.log("one");
+}
+
+const two=()=>{
+    console.log("two");
+}
+
+const three=()=>{
+    console.log("one");
+}
+
+const four=()=>{
+    console.log("four");
+}
+
+export default three;
