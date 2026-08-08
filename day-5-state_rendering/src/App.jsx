0efee96402f9b55466512af117d732decca209a6
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-08-08 21:16:01 +0530

    stared with rendering

---
+++ day-5-state_rendering/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'
+const App=()=>{
+
+  return(
+    <div className="background">
+      <h1 className="red" className="background">Hello</h1>
+    </div>
+  )
+}
+
+export default App;
