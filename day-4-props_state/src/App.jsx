2e8a7490cb9a836c9236afb464ffabc918ef80e6
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-08-08 17:29:20 +0530

    Build a simple counter

--- day-4-props_state/src/App.jsx
+++ day-4-props_state/src/App.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 
+
 const App=()=>{
 
+let [count,setCount]=useState(0)
 
     return(
         <div>
-            <Navbar/>
-            <Hero/>
-            <About/>
-            <Footer/>
+            <h2>Count is -{count}</h2>
+            <button onClick={()=>{
+                setCount(count+1)
+            }}>Increment</button>
         </div>
     )
 }
